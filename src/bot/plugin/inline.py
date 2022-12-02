61b0f4bdbd917055e42a61e33c1cf26ff35765c8
Fumaz <mail@fumaz.dev>
2022-12-02 19:59:00 +0100

    exception handler

--- src/bot/plugin/inline.py
+++ src/bot/plugin/inline.py
@@ -2,33 +2,48 @@ import random
 
 from PIL import Image, ImageOps
 from generativepy.color import Color
-from pyrogram import Client
-from pyrogram.types import InlineQuery, InlineQueryResultPhoto
 from generativepy.formulas import rasterise_formula
+from pyrogram import Client
+from pyrogram.types import InlineQuery, InlineQueryResultPhoto, InlineQueryResultArticle, InputTextMessageContent
 
 
 @Client.on_inline_query()
 async def on_inline_query(_: Client, query: InlineQuery):
-    random_file_name = str(random.randint(100000, 999999))
-    output = rasterise_formula(f"/usr/src/app/{random_file_name}", query.query, Color(0, 0, 0), 1000)
+    try:
+        random_file_name = str(random.randint(100000, 999999))
+        output = rasterise_formula(f"/usr/src/app/{random_file_name}", query.query, Color(0, 0, 0), 1000)
 
-    image = Image.open(f"/usr/src/app/{random_file_name}.png")
-    image = ImageOps.expand(image, border=50, fill="white")
+        image = Image.open(f"/usr/src/app/{random_file_name}.png")
+        image = ImageOps.expand(image, border=50, fill="white")
 
-    new_image = Image.new("RGBA", image.size, "WHITE")
-    new_image.paste(image, (0, 0), image)
-    new_image.convert("RGB").save(f"/usr/src/app/{random_file_name}.png", "PNG")
+        new_image = Image.new("RGBA", image.size, "WHITE")
+        new_image.paste(image, (0, 0), image)
+        new_image.convert("RGB").save(f"/usr/src/app/{random_file_name}.png", "PNG")
 
-    await query.answer(
-        results=[
-            InlineQueryResultPhoto(
-                photo_url=f'https://api.fumaz.dev/latex/{random_file_name}.png',
-                thumb_url=f'https://api.fumaz.dev/latex/{random_file_name}.png',
-                title='Your LaTeX is ready!',
-                description='Click to view your LaTeX image.',
-                caption='🤖 Powered by @LaTeXifyBot'
-            )
-        ],
-        cache_time=0,
-        is_personal=True
-    )
+        await query.answer(
+            results=[
+                InlineQueryResultPhoto(
+                    photo_url=f'https://api.fumaz.dev/latex/{random_file_name}.png',
+                    thumb_url=f'https://api.fumaz.dev/latex/{random_file_name}.png',
+                    title='Your LaTeX is ready!',
+                    description='Click to view your LaTeX image.',
+                    caption='🤖 Powered by @LaTeXifyBot'
+                )
+            ],
+            cache_time=0,
+            is_personal=True
+        )
+    except:
+        await query.answer(
+            results=[
+                InlineQueryResultArticle(
+                    title='An error has occurred!',
+                    description="We couldn't generate your LaTeX.",
+                    input_message_content=InputTextMessageContent(
+                        message_text='An error has occurred!\nWe couldn\'t generate your LaTeX.'
+                    )
+                )
+            ],
+            cache_time=0,
+            is_personal=True
+        )
