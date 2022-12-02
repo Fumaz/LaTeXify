6009da3a5cb44539ca2f004d753f463467ba58ad
Fumaz <mail@fumaz.dev>
2022-12-02 19:48:27 +0100

    done

--- src/bot/plugin/inline.py
+++ src/bot/plugin/inline.py
@@ -8,20 +8,19 @@ from generativepy.formulas import rasterise_formula
 
 @Client.on_inline_query()
 async def on_inline_query(_: Client, query: InlineQuery):
-    output = rasterise_formula("latex", query.query, Color(0, 0, 0))
-    print("Output:", output, flush=True)
-    # output = pnglatex(query.query, output=f'/usr/src/app/images/{random_file_name}')
+    random_file_name = str(random.randint(100000, 999999))
+    output = rasterise_formula(random_file_name, query.query, Color(0, 0, 0))
 
-    # await query.answer(
-    #     results=[
-    #         InlineQueryResultPhoto(
-    #             photo_url=f'https://api.fumaz.dev/latex/{random_file_name}',
-    #             thumb_url=f'https://api.fumaz.dev/latex/{random_file_name}',
-    #             title='Your LaTeX is ready!',
-    #             description='Click to view your LaTeX image.',
-    #             caption='🤖 Powered by @LaTeXifyBot'
-    #         )
-    #     ],
-    #     cache_time=0,
-    #     is_personal=True
-    # )
+    await query.answer(
+        results=[
+            InlineQueryResultPhoto(
+                photo_url=f'https://api.fumaz.dev/latex/{random_file_name}.png',
+                thumb_url=f'https://api.fumaz.dev/latex/{random_file_name}.png',
+                title='Your LaTeX is ready!',
+                description='Click to view your LaTeX image.',
+                caption='🤖 Powered by @LaTeXifyBot'
+            )
+        ],
+        cache_time=0,
+        is_personal=True
+    )
