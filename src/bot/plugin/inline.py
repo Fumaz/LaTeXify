c96bcb2284e1bcc5fae0c7a8321feb2b02ab8096
Fumaz <mail@fumaz.dev>
2022-12-02 20:13:11 +0100

    huh

--- src/bot/plugin/inline.py
+++ src/bot/plugin/inline.py
@@ -1,18 +1,47 @@
 import random
+import subprocess
 
 from PIL import Image, ImageOps
 from generativepy.color import Color
-from generativepy.formulas import rasterise_formula
+from generativepy.formulas import rasterise_formula, tex2, _crop, _remove_ignore_errors
 from pyrogram import Client
 from pyrogram.types import InlineQuery, InlineQueryResultPhoto, InlineQueryResultArticle, InputTextMessageContent
+
+tex1 = '\n'.join([r'\documentclass[preview]{standalone}'
+                  r'\usepackage{amsfonts,amsmath,amssymb,dsfont,mathtools}',
+                  r'\begin{document}',
+                  r'\begin{equation*}'])
+
+
+def rasterize_formula(name, formula, color, dpi):
+    unique_name = "{}-{}".format(name, random.randint(100000, 999999))
+    tex = '\n'.join([tex1, formula, tex2])
+    tex_fn = '{}.tex'.format(unique_name)
+    with open(tex_fn, 'w') as tex_file:
+        tex_file.write(tex)
+    process = subprocess.Popen('latex {}.tex'.format(unique_name), shell=True,
+                               stdout=subprocess.PIPE)
+    process.wait()
+    process = subprocess.Popen('dvipng -T tight -D {} {}.dvi'.format(dpi, unique_name), shell=True,
+                               stdout=subprocess.PIPE)
+    process.wait()
+
+    filename, size = _crop(unique_name, name, color)
+
+    _remove_ignore_errors("{}.aux".format(unique_name))
+    _remove_ignore_errors("{}.log".format(unique_name))
+    _remove_ignore_errors("{}.tex".format(unique_name))
+    _remove_ignore_errors("{}.dvi".format(unique_name))
+    _remove_ignore_errors("{}1.png".format(unique_name))
+
+    return filename, size
 
 
 @Client.on_inline_query()
 async def on_inline_query(_: Client, query: InlineQuery):
     try:
         random_file_name = str(random.randint(100000, 999999))
-        output = rasterise_formula(f"/usr/src/app/{random_file_name}", query.query, Color(0, 0, 0), 1000,
-                                   packages=['amsfonts', 'amsmath', 'amssymb', 'bm', 'cancel', 'color', 'esint'])
+        output = rasterize_formula(f"/usr/src/app/{random_file_name}", query.query, Color(0, 0, 0), 1000)
 
         image = Image.open(f"/usr/src/app/{random_file_name}.png")
         image = ImageOps.expand(image, border=50, fill="white")
